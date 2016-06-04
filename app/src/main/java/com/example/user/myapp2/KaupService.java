272b3e7ce0b479910cfa794693cfdf8c264505cf
hwalwoo <hwalwoo@naver.com>
2016-06-04 11:17:45 +0900

    2016-06-04 Kaup 프로그램 작성

---
+++ app/src/main/java/com/example/user/myapp2/KaupService.java
@@ -0,0 +1,7 @@
+package com.example.user.myapp2;
+
+/**
+ * Created by USER on 2016-06-04.
+ */
+public class KaupService {
+}
--- app/src/main/java/com/example/user/myapp2/MainActivity.java
+++ app/src/main/java/com/example/user/myapp2/MainActivity.java
@@ -1,52 +1,66 @@
 package com.example.user.myapp2;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    EditText etName;
+    EditText etWeight;
+    EditText etHeight;
+    Button btRun;
+    TextView tvResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setTitle("비만지수 계산 프로그램(Kaup)");
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        etName = (EditText) findViewById(R.id.etName);
+        etWeight = (EditText) findViewById(R.id.etWeight);
+        etHeight = (EditText) findViewById(R.id.etHeight);
+        btRun = (Button) findViewById(R.id.btRun);
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btRun.setOnTouchListener(new View.OnTouchListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public boolean onTouch(View v, MotionEvent event) {
+                String vName = etName.getText().toString();
+                double vWeight = Double.parseDouble(etWeight.getText().toString());
+                double vHeight = Double.parseDouble(etHeight.getText().toString());
+                int idx = (int)(vWeight/(vHeight*vHeight)*10000);
+
+                if(idx > 30){
+                    tvResult.setText("비만");
+                }else if(idx >= 24){
+                    tvResult.setText("과체중");
+                }else if(idx >= 20){
+                    tvResult.setText("정상");
+                }else if(idx >= 15){
+                    tvResult.setText("저체중");
+                }else if(idx >= 13){
+                    tvResult.setText("마름");
+                }else if(idx >= 10){
+                    tvResult.setText("영양실조");
+                }else if(idx < 10){
+                    tvResult.setText("소모증");
+                }else{
+                    tvResult.setText("측정불가");
+                }
+                return false;
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
