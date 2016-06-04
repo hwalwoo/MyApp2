27257d42cf82ab44b8f8997b9623b28984fe597e
hwalwoo <hwalwoo@naver.com>
2016-06-04 15:50:14 +0900

    2016-06-04 Kaup 프로그램 작성-모델작성(Toast, 화면전환 초반)

---
+++ app/src/main/java/com/example/user/myapp2/calc/CalcActivity.java
@@ -0,0 +1,75 @@
+package com.example.user.myapp2.calc;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.user.myapp2.R;
+
+public class CalcActivity extends Activity implements View.OnClickListener{
+
+    CalcService service;
+
+    EditText etVal1;
+    EditText etVal2;
+
+    Button btn1;
+    Button btn2;
+    Button btn3;
+    Button btn4;
+    Button btn5;
+
+    TextView tvResult;
+
+    int num1, num2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+        setTitle("==계산기 프로그램==");
+
+        service = new CalcServiceImpl();
+
+        etVal1 = (EditText) findViewById(R.id.etVal1);
+        etVal2 = (EditText) findViewById(R.id.etVal2);
+
+        btn1 = (Button) findViewById(R.id.btn1);
+        btn2 = (Button) findViewById(R.id.btn2);
+        btn3 = (Button) findViewById(R.id.btn3);
+        btn4 = (Button) findViewById(R.id.btn4);
+        btn5 = (Button) findViewById(R.id.btn5);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        num1 = Integer.parseInt(etVal1.getText().toString());
+        num2 = Integer.parseInt(etVal2.getText().toString());
+
+        switch (v.getId()){
+            case R.id.btn1: service.plus(num1, num2); break;
+            case R.id.btn2: service.minus(num1, num2); break;
+            case R.id.btn3: service.multi(num1, num2); break;
+            case R.id.btn4: service.divide(num1, num2); break;
+            case R.id.btn5: service.nmg(num1, num2); break;
+        }
+    }
+}
